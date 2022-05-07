05c8cd43c279b76559cc7d9b3c2a2337d95cd495
Rohin1212 <rohinchopra1212@gmail.com>
2022-05-07 21:45:22 +1000

    finished the project

---
+++ advice-generator-app/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyan: '#cee3e9',
+        'neon-green': '#52ffa8',
+        'grayish-blue': '#4e5d73',
+        'dark-grayish-glue': '#323a49',
+        'dark-blue': '#1f2632',
+        linkedin: '#0077b5',
+        github: '#171515',
+      },
+    },
+  },
+  plugins: [],
+};
