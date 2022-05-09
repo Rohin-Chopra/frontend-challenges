74ff8c7854964a5afa3b595849ad6e2705d23f9f
Rohin1212 <rohinchopra1212@gmail.com>
2022-05-09 20:16:23 +1000

    finished gd and photography sections

--- sunnyside-agency-landing-page/lib/home/index.js
+++ sunnyside-agency-landing-page/lib/home/index.js
@@ -3,7 +3,13 @@ import Image from 'next/image';
 import { Fragment } from 'react';
 import Navbar from '../../components/Navbar';
 import TwoColSection from './components/TwoColSection';
-import { StyledHeader } from './style';
+import {
+  StyledGraphicDesignContainer,
+  StyledPhotographyContainer,
+  StyledHeader,
+  StyledGraphicDesignAndPhotographyContainer,
+  StyledTwoColSection,
+} from './style';
 
 const Home = () => {
   return (
@@ -30,6 +36,23 @@ const Home = () => {
         isReverse
         buttonColor="hsl(7, 99%, 70%)"
       />
+      <StyledTwoColSection>
+        <StyledGraphicDesignContainer>
+          <h2>Graphic Design</h2>
+          <p>
+            Great design makes you memorable. We deliver artwork that
+            underscores your brand message and captures clients&apos; attention
+          </p>
+        </StyledGraphicDesignContainer>
+        <StyledPhotographyContainer>
+          <h2>Photography</h2>
+          <p>
+            Increase your credibility by getting the most stunning, high-quality
+            photos that improve your business image.
+          </p>
+        </StyledPhotographyContainer>
+        <div></div>
+      </StyledTwoColSection>
     </Fragment>
   );
 };
--- sunnyside-agency-landing-page/lib/home/style.js
+++ sunnyside-agency-landing-page/lib/home/style.js
@@ -25,13 +25,42 @@ export const StyledTwoColSection = styled.section`
   display: grid;
   grid-template-columns: repeat(1, minmax(0, 1fr));
   @media (min-width: 992px) {
-    ${(props) =>
-      props.isReverse &&
-      css`
-        > div:first-of-type {
-          order: 2;
-        }
-      `}
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
+`;
+
+export const StyledGDAndPhotographyBase = styled.div`
+  height: 400px;
+  background-size: cover;
+  width: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 4rem;
+  h2 {
+    font-family: 'Fraunces', serif;
+    font-weight: 800;
+    margin-bottom: 1.5rem;
+  }
+  p {
+    line-height: 1.5;
+  }
+  @media (min-width: 992px) {
+    height: 550px;
+    p {
+      max-width: 50%;
+    }
+  }
+`;
+
+export const StyledGraphicDesignContainer = styled(StyledGDAndPhotographyBase)`
+  background-image: url('/desktop/image-graphic-design.jpg');
+  color: hsl(167, 40%, 24%);
+`;
+
+export const StyledPhotographyContainer = styled(StyledGDAndPhotographyBase)`
+  background-image: url('/desktop/image-photography.jpg');
+  color: hsl(198, 62%, 26%);
 `;
